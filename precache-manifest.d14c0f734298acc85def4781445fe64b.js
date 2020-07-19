82192c76c9b7faa06f45410994355ab9b0fc1116
Середин Антон <55971481+NinetiesFiasco@users.noreply.github.com>
2020-07-19 21:59:00 +0300

    Updates

---
+++ precache-manifest.d14c0f734298acc85def4781445fe64b.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfb0f0d2113a94152a7e5db0e05dc55",
+    "url": "/SocSet/index.html"
+  },
+  {
+    "revision": "567381cf4e31385c8c48",
+    "url": "/SocSet/static/css/3.9ee09c10.chunk.css"
+  },
+  {
+    "revision": "74e3df7db576a3398e1b",
+    "url": "/SocSet/static/css/main.0c9b3116.chunk.css"
+  },
+  {
+    "revision": "ec53bd250bdcdb588d3a",
+    "url": "/SocSet/static/js/2.81d8cdce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/SocSet/static/js/2.81d8cdce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "567381cf4e31385c8c48",
+    "url": "/SocSet/static/js/3.f4d3bb36.chunk.js"
+  },
+  {
+    "revision": "74e3df7db576a3398e1b",
+    "url": "/SocSet/static/js/main.bcbc3555.chunk.js"
+  },
+  {
+    "revision": "aa46ba5299f2722cd5fe",
+    "url": "/SocSet/static/js/runtime-main.15ccd04c.js"
+  },
+  {
+    "revision": "e3eca74d0d77d7299725c27949c59ebd",
+    "url": "/SocSet/static/media/rings.e3eca74d.svg"
+  }
+]);
